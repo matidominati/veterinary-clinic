ca690ec9c31b122cf783976eedaa662881b0bdd2
matidominati <141762711+matidominati@users.noreply.github.com>
2023-12-18 01:02:59 +0100

    [EN-11111] check something (#2)
    * [EN-11111] check something
    
    * [EN-11111] check something v2
    
    * [EN-11111] check something v3
    
    * [EN-11111] check something v4
    
    * [EN-1111444] check something v5

---
+++ src/main/java/com/github/matidominati/veterinaryclinic/controller/VisitController.java
@@ -0,0 +1,28 @@
+package com.github.matidominati.veterinaryclinic.controller;
+
+import com.github.matidominati.veterinaryclinic.model.Visit;
+import com.github.matidominati.veterinaryclinic.service.VisitService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/visits")
+public class VisitController {
+
+    private final VisitService visitService;
+
+    @GetMapping("/statistics")
+    public Map<String, Object> getMonthlyStatistics(@RequestParam int month, @RequestParam int year) {
+       return visitService.getMonthlyStatistics(month, year);
+    }
+
+    @PostMapping
+    public Visit createVisit (@RequestBody LocalDateTime visitDateTime){
+        return visitService.createVisit(visitDateTime);
+    }
+}
+alalalallllllll,,,,,,,,,akakakakakak
